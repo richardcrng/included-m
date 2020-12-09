51d09467eed5f36bb567a7e33c089f8b7540b90c
richardcrng@gmail.com <richardcrng@gmail.com>
2020-12-09 19:14:40 +0000

    Add main CTA

--- src/modules/Course/CourseDetails.tsx
+++ src/modules/Course/CourseDetails.tsx
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components'
 import {
+  IonButton,
   IonButtons,
+  IonCard,
+  IonCardContent,
+  IonContent,
   IonToolbar,
 } from '@ionic/react';
 import {
@@ -29,35 +33,70 @@ const Message = styled.p`
   font-weight: bold;
 `
 
+const Container = styled.div`
+  margin: 1rem;
+`
+
+const MainCTAContent = styled(IonCardContent)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 1.15rem;
+  }
+
+  ion-button {
+    width: 100%
+  }
+`
+
 interface Props {
   message?: string;
 }
 
 function CourseDetails() {
   return (
-    <IonToolbar>
-      <Buttons slot='start'>
-        <IoArrowBack
-          onClick={() => {
-            console.log('back')
-          }}
-          size={24}
-        />
-      </Buttons>
-      <Buttons slot='end'>
-        <IoInformationCircleOutline
-          size={24}
-          onClick={() => {
-            window.alert("This is a proof-of-concept for Included M. It's a work in progress!")
-          }}
-        />
-      </Buttons>
-      <Title>
-        <Message>
-          Course Details
-        </Message>
-      </Title>
-    </IonToolbar>
+    <>
+      <IonToolbar>
+        <Buttons slot='start'>
+          <IoArrowBack
+            onClick={() => {
+              console.log('back')
+            }}
+            size={24}
+          />
+        </Buttons>
+        <Buttons slot='end'>
+          <IoInformationCircleOutline
+            size={24}
+            onClick={() => {
+              window.alert("This is a proof-of-concept for Included M. It's a work in progress!")
+            }}
+          />
+        </Buttons>
+        <Title>
+          <Message>
+            Course Details
+          </Message>
+        </Title>
+      </IonToolbar>
+      <IonContent>
+        <Container>
+          <h1>VC Fundamentals</h1>
+          <p>Learn about the structure of a Venture Capital fund.</p>
+          <IonCard>
+            <MainCTAContent>
+              <p>Ready to begin?</p>
+              <p>Start learning now.</p>
+              <br />
+              <IonButton>Start Course</IonButton>
+            </MainCTAContent>
+          </IonCard>
+        </Container>
+      </IonContent>
+    </>
   )
 }
 
