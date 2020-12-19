fe88d177ca11c7f916ebc816c54dc2e39e9759b8
Richard Ng <richardcrng@gmail.com>
2020-12-19 19:35:23 +0000

    Set up basic data view for test

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,3 +1,4 @@
+import { IonContent, IonHeader, IonText, IonToolbar } from "@ionic/react";
 import React from "react";
 import Activity from "./models/Activity";
 import db from "./models/db";
@@ -10,12 +11,6 @@ function Test() {
       const activity = await Activity.findById("FKBJJDcuVMyyLKCyhxao");
       console.log("found activity", activity, activity && activity.toObject());
       setState(activity);
-      const activityTwoSnapshot = await db
-        .collection("activities")
-        .doc("FKBJJDcuVMyyLKCyhxao")
-        .get();
-      const activityTwoData = activityTwoSnapshot.data();
-      console.log("activity two data", activityTwoData);
     };
 
     fetchActivity();
@@ -24,12 +19,28 @@ function Test() {
   if (!state) {
     return <div>Looking for data...</div>;
   } else {
+    console.log("set data", state);
     return (
-      <div>
-        <p>Message</p>
-        <p>{JSON.stringify(state.toObject(), null, 2)}</p>
-        <pre>{JSON.stringify(state.toObject(), null, 2)}</pre>
-      </div>
+      <>
+        <IonToolbar>
+          <IonHeader>Data found</IonHeader>
+        </IonToolbar>
+        <IonContent>
+          <IonText color="white">
+            {JSON.stringify(state.toObject(), null, 2)}
+          </IonText>
+          <div style={{ margin: "1rem" }}>
+            <h1>Show stuff</h1>
+            <div>
+              <p>Message</p>
+              <p>{JSON.stringify(state.toObject(), null, 2)}</p>
+              <div style={{ color: "white" }}>
+                {JSON.stringify(state.toObject(), null, 2)}
+              </div>
+            </div>
+          </div>
+        </IonContent>
+      </>
     );
   }
 }
