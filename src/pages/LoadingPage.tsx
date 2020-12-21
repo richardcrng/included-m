40986049a67feeaf8d35056b3c9edb94958c39d4
Richard Ng <richardcrng@gmail.com>
2020-12-21 06:46:54 +0000

    Add background of white to LessonContinueButton

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,74 +1,71 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import {
   IonButtons,
   IonContent,
+  IonProgressBar,
+  IonSkeletonText,
+  IonSpinner,
   IonToolbar,
-} from '@ionic/react';
-import {
-  IoArrowBack,
-  IoInformationCircleOutline
-} from 'react-icons/io5'
-import { useHistory } from 'react-router';
+} from "@ionic/react";
+import { IoArrowBack, IoInformationCircleOutline } from "react-icons/io5";
+import { useHistory } from "react-router";
 
 const Buttons = styled(IonButtons)`
   margin: 0 1rem;
-`
+`;
 
 const Title = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const Message = styled.p`
   margin: 0;
   text-align: center;
   font-size: 1rem;
   font-weight: bold;
-`
+`;
 
 const Container = styled.div`
-  margin: 1rem;
-`
+  margin: 2rem;
+`;
 
 function LoadingPage() {
-
-  const history = useHistory()
+  const history = useHistory();
 
   return (
     <>
       <IonToolbar>
-        <Buttons slot='start'>
+        <Buttons slot="start">
           <IoArrowBack
             onClick={() => {
-              history.goBack()
+              history.goBack();
             }}
             size={24}
           />
         </Buttons>
-        <Buttons slot='end'>
-          <IoInformationCircleOutline
-            size={24}
-            onClick={() => {
-              window.alert("This is a proof-of-concept for Included M. It's a work in progress!")
-            }}
-          />
+        <Buttons slot="end">
+          <IonSpinner name="dots" />
         </Buttons>
         <Title>
-          <Message>
-            Loading...
-          </Message>
+          <Message>Loading...</Message>
         </Title>
       </IonToolbar>
       <IonContent>
+        <IonProgressBar type="indeterminate" />
         <Container>
-          <h1>Loading from database...</h1>
+          <IonSkeletonText animated style={{ width: "60%" }} />
+          <IonSkeletonText animated />
+          <IonSkeletonText animated style={{ width: "88%" }} />
+          <IonSkeletonText animated style={{ width: "70%" }} />
+          <IonSkeletonText animated style={{ width: "60%" }} />
         </Container>
       </IonContent>
     </>
-  )
+  );
 }
 
-export default LoadingPage
+export default LoadingPage;
--- src/routes/lesson/components/LessonContinueButton.tsx
+++ src/routes/lesson/components/LessonContinueButton.tsx
@@ -1,37 +1,37 @@
-import React, { useContext } from 'react'
-import { IonButton } from '@ionic/react'
-import { LessonContext } from '../LessonPageView'
+import React, { useContext } from "react";
+import { IonButton, IonFooter } from "@ionic/react";
+import { LessonContext } from "../LessonPageView";
 
 interface Props {
-  disabled?: boolean
+  disabled?: boolean;
 }
 
-function LessonContinueButton({
-  disabled
-}: Props) {
+function LessonContinueButton({ disabled }: Props) {
   const {
     dispatch,
     actions,
-    state: { activities, currentIdx }
-  } = useContext(LessonContext)
+    state: { activities, currentIdx },
+  } = useContext(LessonContext);
 
   return (
-    <IonButton
-      color={disabled ? 'medium' : 'primary'}
-      disabled={disabled}
-      expand='full'
-      size='large'
-      onClick={() => {
-        if (currentIdx < activities.length - 1) {
-          dispatch(actions.currentIdx.create.increment())
-        } else {
-          window.alert("You've reached the end of the demo!")
-        }
-      }}
-    >
-      Continue
-    </IonButton>
-  )
+    <IonFooter style={{ backgroundColor: "white" }}>
+      <IonButton
+        color={disabled ? "medium" : "primary"}
+        disabled={disabled}
+        expand="full"
+        size="large"
+        onClick={() => {
+          if (currentIdx < activities.length - 1) {
+            dispatch(actions.currentIdx.create.increment());
+          } else {
+            window.alert("You've reached the end of the demo!");
+          }
+        }}
+      >
+        Continue
+      </IonButton>
+    </IonFooter>
+  );
 }
 
-export default LessonContinueButton
+export default LessonContinueButton;
