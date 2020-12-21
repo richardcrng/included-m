caf6e69880e91a7922c05c2af5cd4e21c5495d83
Richard Ng <richardcrng@gmail.com>
2020-12-21 07:08:04 +0000

    Change to ReactSwipeableViews

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -4,20 +4,18 @@ import {
   IonCardContent,
   IonCardHeader,
   IonCardSubtitle,
-  IonCardTitle,
   IonCol,
   IonContent,
   IonFooter,
   IonGrid,
   IonImg,
   IonRow,
-  IonSlide,
-  IonSlides,
   IonText,
   IonToolbar,
 } from "@ionic/react";
 import React from "react";
 import styled from "styled-components";
+import SwipeableViews from "react-swipeable-views";
 import { DEFAULT_COURSE_ID, INCLUDED_VC_LOGO } from "../constants";
 import creandum from "../assets/partners/creandum-logo.png";
 import daphni from "../assets/partners/daphni-logo.png";
@@ -111,89 +109,83 @@ function HomePage() {
         </div>
       </IonToolbar>
       <IonContent className="ion-padding">
-        <IonSlides pager style={{ height: "100%" }}>
-          <IonSlide>
-            <SlideBody>
-              <h1>Learn venture, for free</h1>
-              <IonCard>
-                <IonCardHeader>
-                  <IonCardSubtitle>
-                    Venture capital is <i>now open</i>.
-                  </IonCardSubtitle>
-                </IonCardHeader>
-                <IonCardContent>
-                  <IonText>
-                    <p style={{ fontSize: "16px" }}>
-                      Included M is a <b>fully open-source</b> learning pathway
-                      in venture capital.
-                    </p>
-                    <Callout>
-                      <div>
-                        <p>
-                          <span>🔑</span> Anyone can sign up
-                        </p>
-                        <p style={{ fontSize: "16px", textAlign: "left" }}>
-                          <span>📲</span> Learn on the go
-                        </p>
-                        <p style={{ fontSize: "16px", textAlign: "left" }}>
-                          <span>♾️</span> Free forever
-                        </p>
-                      </div>
-                    </Callout>
-                    <p>
-                      Brought to life by{" "}
-                      <a href="https://included.vc" target="_blank">
-                        Included VC
-                      </a>
-                    </p>
-                  </IonText>
-                </IonCardContent>
-              </IonCard>
-            </SlideBody>
-          </IonSlide>
-          <IonSlide>
-            <SlideBody>
-              <h1>Supported by leading funds</h1>
-              <IonCard>
-                {/* <IonCardHeader style={{ paddingBottom: "0" }}>
+        <SwipeableViews enableMouseEvents>
+          <SlideBody>
+            <h1>Learn venture, for free</h1>
+            <IonCard>
+              <IonCardHeader>
+                <IonCardSubtitle>
+                  Venture capital is <i>now open</i>.
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonText>
+                  <p style={{ fontSize: "16px" }}>
+                    Included M is a <b>fully open-source</b> learning pathway in
+                    venture capital.
+                  </p>
+                  <Callout>
+                    <div>
+                      <p>
+                        <span>🔑</span> Anyone can sign up
+                      </p>
+                      <p style={{ fontSize: "16px", textAlign: "left" }}>
+                        <span>📲</span> Learn on the go
+                      </p>
+                      <p style={{ fontSize: "16px", textAlign: "left" }}>
+                        <span>♾️</span> Free forever
+                      </p>
+                    </div>
+                  </Callout>
+                  <p>
+                    Brought to life by{" "}
+                    <a href="https://included.vc" target="_blank">
+                      Included VC
+                    </a>
+                  </p>
+                </IonText>
+              </IonCardContent>
+            </IonCard>
+          </SlideBody>
+          <SlideBody>
+            <h1>Supported by leading funds</h1>
+            <IonCard>
+              {/* <IonCardHeader style={{ paddingBottom: "0" }}>
                   <IonCardSubtitle>Included VC Partners:</IonCardSubtitle>
                 </IonCardHeader> */}
-                <IonGrid>
-                  <ImageRow indices={[0, 1, 2]} />
-                  <ImageRow indices={[3, 4, 5]} />
-                  <ImageRow indices={[6, 7, 8]} />
-                  <ImageRow indices={[10, 9, 11]} />
-                </IonGrid>
-              </IonCard>
-            </SlideBody>
-          </IonSlide>
-          <IonSlide>
-            <SlideBody>
-              <h1>Change the face of venture</h1>
-              <IonCard>
-                <IonCardContent>
-                  <p>
-                    The Included VC mission is to{" "}
-                    <b>change the face of the VC industry</b>.
-                  </p>
-                  <br />
-                  <p>
-                    That's why we built{" "}
-                    <a href="https://included.vc" target="_blank">
-                      a fully-funded Fellowship
-                    </a>
-                    . Now,{" "}
-                    <b>
-                      we're opening up our knowledge to <i>everyone</i>
-                    </b>{" "}
-                    and <i>anyone</i>
-                    with a phone and internet.
-                  </p>
-                </IonCardContent>
-              </IonCard>
-            </SlideBody>
-          </IonSlide>
-        </IonSlides>
+              <IonGrid>
+                <ImageRow indices={[0, 1, 2]} />
+                <ImageRow indices={[3, 4, 5]} />
+                <ImageRow indices={[6, 7, 8]} />
+                <ImageRow indices={[10, 9, 11]} />
+              </IonGrid>
+            </IonCard>
+          </SlideBody>
+          <SlideBody>
+            <h1>Change the face of venture</h1>
+            <IonCard>
+              <IonCardContent>
+                <p>
+                  The Included VC mission is to{" "}
+                  <b>change the face of the VC industry</b>.
+                </p>
+                <br />
+                <p>
+                  That's why we built{" "}
+                  <a href="https://included.vc" target="_blank">
+                    a fully-funded Fellowship
+                  </a>
+                  . Now,{" "}
+                  <b>
+                    we're opening up our knowledge to <i>everyone</i>
+                  </b>{" "}
+                  and <i>anyone</i>
+                  with a phone and internet.
+                </p>
+              </IonCardContent>
+            </IonCard>
+          </SlideBody>
+        </SwipeableViews>
       </IonContent>
       <IonFooter className="ion-no-border" style={{ backgroundColor: "white" }}>
         <IonButton routerLink={`/course/${DEFAULT_COURSE_ID}`} expand="full">
