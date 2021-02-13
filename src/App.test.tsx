544627d36d7b05a095b820fa13996709e34f931d
Richard Ng <richardcrng@gmail.com>
2021-02-13 16:46:11 +0000

    Delete test files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders without crashing', () => {
-  expect(4).toBe(4)
-  // const { baseElement } = render(<App />);
-  // expect(baseElement).toBeDefined();
-});
