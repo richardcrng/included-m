0f19e5c0d09998eccc30ff174763fee27b3d3889
richardcrng@gmail.com <richardcrng@gmail.com>
2021-02-14 12:41:38 +0000

    Add tests for isSelectAnAnswerActivity

--- src/content/validators/activity-validation.test.ts
+++ src/content/validators/activity-validation.test.ts
@@ -1,4 +1,8 @@
-import { isContentBlock, isReadActivity } from "./activity-validation";
+import {
+  isContentBlock,
+  isReadActivity,
+  isSelectAnAnswerActivity,
+} from "./activity-validation";
 
 describe("isContentBlock", () => {
   describe("happy path", () => {
@@ -44,9 +48,36 @@ describe("isReadActivity", () => {
         })
       ).toBe(true);
     });
+
+    it("permits a string blocks array", () => {
+      expect(
+        isReadActivity({
+          activityType: "read",
+          blocks: ["hello", "world"],
+        })
+      ).toBe(true);
+    });
+
+    it("permits a markdown objects blocks array", () => {
+      expect(
+        isReadActivity({
+          activityType: "read",
+          blocks: [{ markdown: "hello" }, { markdown: "world" }],
+        })
+      ).toBe(true);
+    });
   });
 
   describe("sad path", () => {
+    it("requires activityType to be read", () => {
+      expect(
+        isReadActivity({
+          activityType: "select-an-answer",
+          blocks: [],
+        })
+      ).toBe(false);
+    });
+
     it("requires a blocks key to be present", () => {
       expect(
         isReadActivity({
@@ -69,6 +100,59 @@ describe("isReadActivity", () => {
         isReadActivity({
           type: "read",
           blocks: [],
+        })
+      ).toBe(false);
+    });
+  });
+});
+
+describe("isSelectAnAnswerActivity", () => {
+  describe("happy path", () => {
+    it("Allows one correct and one incorrect answer", () => {
+      expect(
+        isSelectAnAnswerActivity({
+          activityType: "select-an-answer",
+          blocks: ["What is 1 + 1?"],
+          answers: [
+            { text: "2", isCorrect: true },
+            { text: "3", isCorrect: false },
+          ],
+        })
+      ).toBe(true);
+    });
+
+    it("Allows one correct and multiple incorrect answers", () => {
+      expect(
+        isSelectAnAnswerActivity({
+          activityType: "select-an-answer",
+          blocks: ["What is 1 + 1?"],
+          answers: [
+            { text: "1", isCorrect: false },
+            { text: "2", isCorrect: true },
+            { text: "3", isCorrect: false },
+            { text: "4", isCorrect: false },
+          ],
+        })
+      ).toBe(true);
+    });
+  });
+
+  describe("sad path", () => {
+    it("requires an answer key", () => {
+      expect(
+        isSelectAnAnswerActivity({
+          activityType: "select-an-answer",
+          blocks: ["test"],
+        })
+      ).toBe(false);
+    });
+
+    it("requires answers to be formatted correctly", () => {
+      expect(
+        isSelectAnAnswerActivity({
+          activityType: "select-an-answer",
+          blocks: ["test"],
+          answers: ["not an answer"],
         })
       ).toBe(false);
     });
--- src/content/validators/activity-validation.ts
+++ src/content/validators/activity-validation.ts
@@ -1,15 +1,34 @@
 import { isPlainObj } from "ramda-adjunct";
 import {
   ActivityType,
+  AnswerJSON,
   ContentBlockJSON,
   ReadActivityJSON,
+  SelectAnAnswerActivityJSON,
 } from "../types/content-yaml.types";
 import { hasOwnProperties, hasOwnProperty } from "./utils";
 
-export function isArrayOfContentBlocks(
-  maybeArray: unknown
-): maybeArray is ContentBlockJSON[] {
-  return Array.isArray(maybeArray) && maybeArray.every(isContentBlock);
+export function isAnswerBlock(
+  parsedYamlBlock: unknown
+): parsedYamlBlock is AnswerJSON {
+  if (
+    isPlainObj(parsedYamlBlock) &&
+    hasOwnProperties(parsedYamlBlock, ["text", "isCorrect"])
+  ) {
+    return (
+      typeof parsedYamlBlock.text === "string" &&
+      typeof parsedYamlBlock.isCorrect === "boolean"
+    );
+  }
+
+  return false;
+}
+
+export function isArrayOf<T>(
+  maybeArray: unknown,
+  isType: (element: T) => element is T
+): maybeArray is T[] {
+  return Array.isArray(maybeArray) && maybeArray.every(isType);
 }
 
 export function isContentBlock(
@@ -31,7 +50,21 @@ export function isReadActivity(
   if (hasOwnProperties(parsedYaml, ["activityType", "blocks"])) {
     return (
       parsedYaml.activityType === ActivityType.READ &&
-      isArrayOfContentBlocks(parsedYaml.blocks)
+      isArrayOf(parsedYaml.blocks, isContentBlock)
+    );
+  }
+
+  return false;
+}
+
+export function isSelectAnAnswerActivity(
+  parsedYaml: object
+): parsedYaml is SelectAnAnswerActivityJSON {
+  if (hasOwnProperties(parsedYaml, ["activityType", "blocks", "answers"])) {
+    return (
+      parsedYaml.activityType === ActivityType.SELECT_AN_ANSWER &&
+      isArrayOf(parsedYaml.blocks, isContentBlock) &&
+      isArrayOf(parsedYaml.answers, isAnswerBlock)
     );
   }
 
